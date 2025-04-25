58057abb2e9771694cbc19477b09553b85d64524
n8nTeste <koresoluciones@outlook.com>
2025-04-25 18:32:24 -0300

    atulizacao rodape

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <footer className="bg-black bg-opacity-95 backdrop-blur w-full left-0 right-0 bottom-0 py-6 px-2 text-gray-400 text-sm mt-8 fixed md:static z-30">
-      <div className="w-full flex flex-col md:flex-row justify-between items-center gap-2 md:gap-4 px-2 md:px-4">
-        <div className="flex gap-6 mb-2 md:mb-0">
+    <footer className="bg-black bg-opacity-95 w-full py-6 px-2 text-gray-400 text-sm mt-8">
+      <div className="w-full flex flex-col sm:flex-col md:flex-row justify-center md:justify-between items-center gap-3 md:gap-4 px-2 md:px-4 text-center">
+        <div className="flex flex-col sm:flex-col md:flex-row gap-2 md:gap-6 mb-2 md:mb-0 items-center">
           <Link href="/" className="hover:text-ciano-eletrico">Início</Link>
           <Link href="/servicos" className="hover:text-ciano-eletrico">Serviços</Link>
           <Link href="/cursos" className="hover:text-ciano-eletrico">Cursos</Link>
